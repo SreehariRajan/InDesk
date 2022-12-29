2e27ae2d93b8701b9c20f7e59d569c4feb2ac3cf
SreehariRajan <77159058+SreehariRajan@users.noreply.github.com>
2022-12-29 20:24:02 +0530

    login page

---
+++ src/taskmanager/TaskManager.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
+ */
+package taskmanager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Sreehari Rajan
+ */
+public class TaskManager extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        try{
+            Parent root = FXMLLoader.load((getClass().getResource("resources/views/Home.fxml")));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}

b914ff2594e54f241372d8d6093817ac60368ff2
SreehariRajan <77159058+SreehariRajan@users.noreply.github.com>
2022-12-29 20:43:16 +0530

    login page changed

--- src/taskmanager/TaskManager.java
+++ src/taskmanager/TaskManager.java
@@ -19,7 +19,7 @@ public class TaskManager extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         try{
-            Parent root = FXMLLoader.load((getClass().getResource("resources/views/Home.fxml")));
+            Parent root = FXMLLoader.load((getClass().getResource("resources/views/Login.fxml")));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
